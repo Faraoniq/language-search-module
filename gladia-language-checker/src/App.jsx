33d0ab5717921ef064dbb63abfd67aa8d811d704
Faraoniq <ftahri@gladia.io>
2026-01-28 16:12:09 +0100

    Add files via upload

---
+++ gladia-language-checker/src/App.jsx
@@ -0,0 +1,11 @@
+import LanguageSupportChecker from './components/LanguageSupportChecker'
+
+function App() {
+  return (
+    <div style={{ padding: '40px', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <LanguageSupportChecker />
+    </div>
+  )
+}
+
+export default App
